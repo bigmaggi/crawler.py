45b8d70add03fe9471d5fbc8efbfc19b28a0bdb0
bigmaggi <stuffstuff@nightmare.life>
2023-07-14 19:38:43 +0200

    slight modifications

--- search.py
+++ search.py
@@ -1,88 +1,99 @@
 from elasticsearch import Elasticsearch
-from elasticsearch.helpers import scan
-from sklearn.feature_extraction.text import TfidfVectorizer
+import math
 from concurrent.futures import ThreadPoolExecutor
+from sklearn.feature_extraction.text import TfidfVectorizer
 from tqdm import tqdm
-import numpy as np
+from collections import Counter, defaultdict
+from joblib import Parallel, delayed
 
 # Elasticsearch configuration
 ELASTICSEARCH_HOST = "localhost"
 ELASTICSEARCH_PORT = 9200
 ELASTICSEARCH_INDEX = "web_indexer"
 
-# Okapi BM25 parameters
-K1 = 1.2
-B = 0.75
+def bm25(query, document, corpus, k1=1.2, b=0.75):
+    """
+    Calculates the Okapi BM25 score for a query and document in a corpus.
 
-# Create an Elasticsearch client
-client = Elasticsearch(
-    hosts=[{"host": ELASTICSEARCH_HOST, "port": ELASTICSEARCH_PORT, "scheme": "http"}]
-)
+    Args:
+        query (str): The query string.
+        document (str): The document string.
+        corpus (set): A set of document strings in the corpus.
+        k1 (float): A tuning parameter that controls the impact of term frequency.
+        b (float): A tuning parameter that controls the impact of document length.
 
-# Search the documents in Elasticsearch using Okapi BM25 ranking
-def search_documents(client, query, limit=10, num_threads=16):
+    Returns:
+        float: The Okapi BM25 score for the query and document.
+    """
+    # Tokenize the query and document
+    query_tokens = query.split()
+    document_tokens = document.split()
+
+    # Calculate the document length
+    document_length = len(document_tokens)
+
+    # Calculate the average document length in the corpus
+    avg_document_length = sum(len(d.split()) for d in corpus) / len(corpus)
+
+    # Calculate the inverse document frequency for each query term
+    idf = defaultdict(lambda: 0)
+    for token in query_tokens:
+        doc_freq = sum(1 for d in corpus if token in d)
+        idf[token] = math.log((len(corpus) - doc_freq + 0.5) / (doc_freq + 0.5))
+
+    # Calculate the term frequency for each query term in the document
+    tf = Counter(query_tokens)
+
+    # Calculate the Okapi BM25 score
+    score = 0
+    for token in query_tokens:
+        score += idf[token] * ((tf[token] * (k1 + 1)) / (tf[token] + k1 * (1 - b + b * (document_length / avg_document_length))))
+
+    return score
+
+def search_documents(client, query, limit=10, num_threads=4):
     # Get all documents from Elasticsearch
-    documents = scan(client, index=ELASTICSEARCH_INDEX, query={"query": {"match_all": {}}})
+    documents = client.search(index=ELASTICSEARCH_INDEX, body={"query": {"match_all": {}}}, size=limit)["hits"]["hits"]
 
-    # Extract the URLs and contents from the documents
     urls = []
-    contents = []
+    contents = set()
     for doc in documents:
-        url = doc["_source"].get("url")
-        content = doc["_source"].get("content")
+        source = doc["_source"]
+        url = source.get("url")
+        content = source.get("content")
         if url and content:
             urls.append(url)
-            contents.append(content)
-
+            contents.add(content)
+    
     if not urls:
         print("No documents found.")
         return []
-
+    
     # Apply TF-IDF vectorization to the contents
     vectorizer = TfidfVectorizer()
     X = vectorizer.fit_transform(contents)
 
-    # Normalize the query vector
-    query_vector = vectorizer.transform([query])
-    query_vector_normalized = query_vector / np.linalg.norm(query_vector.toarray())
-
-    # Calculate the Okapi BM25 similarity between the normalized query vector and document vectors using multiple threads
-    bm25_similarities = []
-    avgdl = np.mean(X.sum(axis=1))
+    # Calculate the Okapi BM25 score for each document and query pair using multiple threads
+    scores = []
     with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        futures = []
-        for i in tqdm(range(len(urls)), desc="Calculating similarities"):
-            document_vector = X[i].toarray()
-            document_length = np.sum(document_vector)
-            document_vector_normalized = document_vector / document_length
-            future = executor.submit(okapi_bm25_similarity, query_vector_normalized.toarray(), document_vector_normalized, document_length, avgdl)
-            futures.append(future)
-        for i, future in enumerate(tqdm(futures, desc="Collecting results", total=len(futures))):
-            similarity = future.result()
-            bm25_similarities.append((urls[i], similarity))
+        results = Parallel(n_jobs=num_threads, backend="threading")(delayed(bm25)(query, document, contents) for document in tqdm(contents, desc="Calculating scores"))
+        scores = list(zip(urls, results))
 
-    # Sort the results by similarity
-    sorted_results = sorted(bm25_similarities, key=lambda x: x[1], reverse=True)
+    # Sort the results by score
+    sorted_results = [url for url, score in sorted(scores, key=lambda x: x[1], reverse=True)]
 
-    # Return the top 'limit' search results with scores
     return sorted_results[:limit]
 
-# Calculate the Okapi BM25 similarity between the query vector and document vector
-def okapi_bm25_similarity(query_vector, document_vector, document_length, avgdl):
-    term1 = (K1 + 1) * document_vector
-    term2 = K1 * ((1 - B) + B * (document_length / avgdl))
-    similarity = query_vector.dot(term1.T) / (query_vector + term2)
-    return similarity[0, 0]
-
 def main():
+    # Connect to Elasticsearch
+    client = Elasticsearch([{"host": ELASTICSEARCH_HOST, "port": ELASTICSEARCH_PORT, "scheme": "http"}])
+
     # Search for a query
     query = input("Enter a search query: ")
-    search_results = search_documents(client, query, limit=10, num_threads=16)
+    search_results = search_documents(client, query)
     print("Search Results:")
-    for rank, (result, score) in enumerate(search_results, start=1):
-        print(f"Rank: {rank}")
+    for result in search_results:
         print(f"URL: {result}")
-        print(f"Score: {score}")
         print()
 
 if __name__ == "__main__":
